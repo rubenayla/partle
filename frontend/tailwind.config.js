d47558a6ca0b61b7a1efa55bb2ff4d5268472b04
Rubén <ruben.jimenezmejias@gmail.com>
2025-06-21 20:19:40 +0200

    Add css to config file, should remove linter complaining

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   darkMode: 'class',          // html.dark enables dark palette
   content: [
     './index.html',
-    './src/**/*.{js,jsx,ts,tsx}',
+    './src/**/*.{js,jsx,ts,tsx,css}',
   ],
   theme: {
     extend: {
